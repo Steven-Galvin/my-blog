8cbafe6b7b6471ebd367880d8789aafcd9e48d0a
Steven Galvin <steven.john.galvin@gmail.com>
2019-07-21 00:08:15 -0700

    add sessions controller and routes for login and log out

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
 
   root 'pages#index'
+
   get 'about', to: 'pages#about'
   get 'signup',  to: 'users#new'
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
 end
